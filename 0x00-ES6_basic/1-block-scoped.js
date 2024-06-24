36d832de9a577d4b0cb1f279f66699df6395fc6f
MungaSoftwiz <mwachilumobm@gmail.com>
2024-06-24 17:50:21 +0300

    Prevent vars overwritting in conditional block

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    if (task && task2) return task;
+  }
+
+  return [task, task2];
+}
