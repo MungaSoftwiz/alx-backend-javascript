4ece465e47704c78f93056da5f2ebf6205f0d58c
MungaSoftwiz <mwachilumobm@gmail.com>
2024-06-28 01:44:35 +0300

    Implement custom type casting methods

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,39 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError("size must be a number");
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError("location must be a string");
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() { return this._size; }
+
+  set size(val) {
+    if (typeof val !== 'number') {
+      throw new TypeError("size must be a number");
+    }
+    this._size = val;
+  }
+
+  get location() { return this._location; }
+
+  set location(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError("location must be a string");
+    }
+    this._location = val;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
