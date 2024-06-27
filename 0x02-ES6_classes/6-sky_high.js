a1f9e6698ba949c4b697aa56c3b7fa1150f60e67
MungaSoftwiz <mwachilumobm@gmail.com>
2024-06-28 00:25:13 +0300

    Inherit attributes and methods

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
+      throw new TypeError("floors must be of type number");
+    }
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
