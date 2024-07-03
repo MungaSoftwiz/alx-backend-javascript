bf03e54a463ea056f4870a003abdc9557ccc988b
MungaSoftwiz <mwachilumobm@gmail.com>
2024-07-03 03:46:26 +0300

    Refactor assignment of Int8 value at position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,6 +4,6 @@ export default function createInt8TypedArray(length, position, value) {
   }
   const buffer = new ArrayBuffer(length);
   const int8View = new DataView(buffer);
-  int8View[position] = value;
+  int8View.setInt8(position, value);
   return int8View;
 }
