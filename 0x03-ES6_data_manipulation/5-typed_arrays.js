437577af6dee53c25b2cb28a7fb07a749945ae6d
MungaSoftwiz <mwachilumobm@gmail.com>
2024-07-03 03:37:19 +0300

    Return ArrayBuffer with Int8 value at position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
-  const arr = new Int8Array(buffer);
-  arr[position] = value;
-  return buffer;
+  const int8View = new DataView(buffer);
+  int8View[position] = value;
+  return int8View;
 }
