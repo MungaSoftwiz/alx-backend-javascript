ed66aa3c85ef2ecd56b5d3bb3a047ed6ac4b4a76
MungaSoftwiz <mwachilumobm@gmail.com>
2024-08-21 01:26:15 +0300

    Print string argument in stdout

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Task prints a message on STDOUT.
+ * @param {String} msg Message to be displayed.
+ */
+function displayMessage(data) {
+  console.log(data);
+}
+
+module.exports = displayMessage;
