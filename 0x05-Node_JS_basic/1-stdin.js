7864befa8b4dc178f28f0c3ebb71d10db771e184
MungaSoftwiz <mwachilumobm@gmail.com>
2024-08-21 03:52:47 +0300

    Use streams to prompt name user and output

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/**
+ * This script uses streams to prompts the user for their name via stdin and outputs name.
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+
+process.stdin.on('readable', () => {
+  const dataChunk = process.stdin.read();
+
+  if (dataChunk) {
+    process.stdout.write(`Your name is: ${dataChunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
