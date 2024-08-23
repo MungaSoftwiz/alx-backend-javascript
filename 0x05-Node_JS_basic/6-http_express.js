5d8d308fc7e203d1fe7b5a71210eaee60251ca31
MungaSoftwiz <mwachilumobm@gmail.com>
2024-08-24 00:30:49 +0300

    Fix style of module

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const port = 1245;
 const hostname = '127.0.0.1';
@@ -7,6 +8,6 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {});
+app.listen(port, hostname, () => { });
 
 module.exports = app;
